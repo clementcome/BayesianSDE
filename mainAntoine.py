d3e6914c622f09132eaf10cf5d6da6c504ff9d8d
Antoine Leefsma <49612716+Leffee@users.noreply.github.com>
2019-12-13 13:30:20 +0100

    Update mainAntoine.py
    j'ai enlevé les +j-j, les inv-gamma marchent, et j'ai modifié l'initialisation de b

--- mainAntoine.py
+++ mainAntoine.py
@@ -13,6 +13,7 @@ data['delta_date'] = (data['date'] - np.datetime64('1970-01-02'))/np.timedelta64
 
 
 
+
 def main (data,m,Niter) :
   
     
@@ -28,12 +29,11 @@ def main (data,m,Niter) :
     nu= copy.deepcopy(x)
     R=0*x
     Dtau = 1/m
-    b1= np.random.invgamma(4,1)
-    b=[]
-    for k in range (1,N+1):   #initialisation de b
-        b.append(b1^k)
+    b1= 1/(np.random.gamma(4,1))
+    b=[[0]*Niter]*N
+    for k in range (N):   #initialisation de b
         for l in range(Niter):
-            b[k].append(b[k][0])
+            b[k][l]=b1**(k+1)
     
     mu0=0
     theta1 = np.random.normal(mu0,0.01)
@@ -46,29 +46,28 @@ def main (data,m,Niter) :
                 for i in range (N-1):
                     nu[m*j+k][i] = (1+b[l][i]*Dtau) * nu [m*j+k-1][i]
                     
-                    muR= R[m*j+k-1][i] +Dtau*(-R[m*j+k-1][i])/(j+1-j+(k-1)/m)
-                    sigmaR= (j+1-j+k/m)/(j+1-j+(k-1)/m)*Dtau*theta[l]^2*x[m*j+k-1][i]
+                    muR= R[m*j+k-1][i] +Dtau*(-R[m*j+k-1][i])/(1+(k-1)/m)
+                    sigmaR= (1+k/m)/(1+(k-1)/m)*Dtau*theta[l]**2*x[m*j+k-1][i]
                     
-                    R[m*j+k][i]=np.random.normal(muR.sigmaR);
+                    R[m*j+k][i]=np.random.normal(muR,sigmaR);
                 
                 x[m*j+k][i]= nu[m*j+k]+R[m+j+k];
 
         mu=0
         beta=0
-        sigma=b[l]^2/Dtau/N/n/m
+        sigma=b[l]**2/Dtau/N/n/m
         for i in range (l):
-            beta += (x[i][j]/x[i][j]-(1+theta*Dtau))^2
+            beta += (x[i][j]/x[i][j]-(1+theta*Dtau))**2
             for j in range (N):
                mu+= (x[i][j]/x[i][j]-1)/Dtau
                
-        theta[l+1]= np.random.normal(0.01/(sigma+0.01)*mu + sigma/(sigma+0.01)*mu , (1/0.01+1/sigma)^(-1))
-        b[l+1]= np.random.invgamma(4+n*m/2,1+beta)
+        theta[l+1]= np.random.normal(0.01/(sigma+0.01)*mu + sigma/(sigma+0.01)*mu , (1/0.01+1/sigma)**(-1))
+        b[l+1]= 1/(np.random.gamma(4+n*m/2,1+beta))
         
     X=[k for k in range (1,Niter+1)]
     plt.plot(X,theta,X,b)
     plt.show()
     return 0
-
 
 
 
