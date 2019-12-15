19e4a87fba4611cfb7be6511248255f97044da1d
Antoine Leefsma <49612716+Leffee@users.noreply.github.com>
2019-12-16 00:00:44 +0100

    Update mainAntoine.py
    ajout de muT, modif de l'initialisation des tableaux, optimisation dans le gibbs sampler

--- mainAntoine.py
+++ mainAntoine.py
@@ -26,49 +26,68 @@ def main (data,m,Niter) :
     
     N= len(tickers)
     n=len(dates)
-    nu= copy.deepcopy(x)
-    R=0*x
+    eta= copy.deepcopy(x)
+    R= np.zeros(((n-1)*m+1,N))
     Dtau = 1/m
-    b1= 1/(np.random.gamma(4,1))
-    b=[[0]*Niter]*N
-    for k in range (N):   #initialisation de b
-        for l in range(Niter):
-            b[k][l]=b1**(k+1)
     
+    #initialisation de b
+    b1= 1/(np.random.gamma(4,1))
+    b= np.zeros((N,Niter))
+    for k in range (Niter):   
+        for l in range(N):
+            b[k][l]=b1**(l+1)
+    print("test)")
+    #initialisation theta
     mu0=0
-    theta1 = np.random.normal(mu0,0.01)
-    theta = [theta1]*Niter
+    theta = np.zeros((1,Niter))
+    theta[0]=np.random.normal(mu0,0.01)
     
     
     for l in range (Niter):
         for j in range (n-2):
             for k in range (m-1):
                 for i in range (N-1):
-                    nu[m*j+k][i] = (1+b[l][i]*Dtau) * nu[m*j+k-1][i]
+                    eta[m*j+k][i] = (1+b[l][i]*Dtau) * eta[m*j+k-1][i]
+                                        
                     
-                    muR= R[m*j+k-1][i] +Dtau*(-R[m*j+k-1][i])/(1+(k-1)/m)
-                    sigmaR= (1+k/m)/(1+(k-1)/m)*Dtau*theta[l]**2*x[m*j+k-1][i]
+                    muR= R[m+j+k-1][i] +Dtau*(-R[m+j+k-1][i])/(1+(k-1)/m)
+                    sigmaR= (m+k)/(m+k-1)*Dtau*theta[l]**2*x[m+j+k-1][i]**2
                     
-                    R[m*j+k][i]=np.random.normal(muR,sigmaR);
+                    R[m+j+k][i]=np.random.normal(muR,sigmaR);
                 
-                x[m*j+k]= nu[m*j+k]+R[m+j+k];
+                x[m+j+k]= eta[m+j+k]+R[m+j+k];
 
-        mu=0
+
+        mui=0
+        tau=0
         beta=0
-        sigma=b[l]**2/Dtau/N/n/m
-        for i in range (l):
-            beta += (x[i][j]/x[i][j]-(1+theta*Dtau))**2
-            for j in range (N):
-               mu+= (x[i][j]/x[i][j]-1)/Dtau
-               
-        theta[l+1]= np.random.normal(0.01/(sigma+0.01)*mu + sigma/(sigma+0.01)*mu , (1/0.01+1/sigma)**(-1))
+        mut=0
+         
+        #calcul de tau* et muT
+        for i in range (N):
+            tau += 1/np.power(b[i],2)
+
+            for j in range (n-1):
+                for k in range (1,m):
+                    
+                    mui+= x[j+k][i]/x[j+k-1][i]
+            mui = mui/Dtau - (1/Dtau)*(n-1)*(m-1)
+            mut += mui*Dtau*n*m/np.power(b[i],2)
+            mui=0
+        tau = Dtau*n*m*tau  
+        
+        # calcul de Beta*
+        for j in range (n-1):
+            for k in range (1,m):
+                beta += np.power(x[j+k][l]/x[j+k-1][l]-(1+theta[l]*Dtau)) #quel indice pour l ?
+        
+        
+        theta[l+1]= np.random.normal(mut/tau, 1/tau)
         b[l+1]= 1/(np.random.gamma(4+n*m/2,1+beta))
         
     X=[k for k in range (1,Niter+1)]
     plt.plot(X,theta,X,b)
     plt.show()
     return 0
-
-
 
 
