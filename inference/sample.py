576c0d1ea199b2debeb9668edadc6c5102276d5e
Antoine Leefsma <49612716+Leffee@users.noreply.github.com>
2019-12-21 18:29:32 +0100

    Update sample.py

--- inference/sample.py
+++ inference/sample.py
@@ -51,6 +51,7 @@ class StockSampler():
         self.init_shape(n_iter)
         self.init_value(m0, s02, a0, b0)
         self.beta = []
+        tau_0 = self.n*self.m/self.b2[0]/self.b2[0]
         for l in tqdm(range(n_iter)):
             for j in range(self.n-1):
                 for k in range(1,self.m):
@@ -65,15 +66,11 @@ class StockSampler():
                 mu_ijk = np.delete(mu_ijk, 0, axis=0)
                 mu_i = np.sum(mu_ijk, axis=0)/(self.n*self.m)
                 tau_i = self.n/ self.b2[l]/(self.n*self.m)
-                tau_star = (np.sum(tau_i) + 1/s02)/(self.N+1)
-                mu_star = (np.sum(mu_i*tau_i) + m0/s02)/(self.N+1)/tau_star
                 beta_ijk = (self.x/np.roll(self.x, 1, axis=1) -1 - self.theta[l]/self.m)**2
                 beta_ijk = np.delete(beta_ijk, 0, axis=0)
                 beta_i_star = np.sum(beta_ijk, axis=0)
-                self.beta.append(beta_i_star)
-                
-                self.theta[l+1]= np.random.normal([mu_star]*self.N, [np.sqrt(1/tau_star)]*self.N)
-                
+                self.beta.append(beta_i_star)              
+                self.theta[l+1]= np.random.normal((mu_i + m0)/(tau_i+tau_0), 1/(tau_i+ tau_0))        
                 proposed_b2 = 1/np.random.gamma(a0 + 1/2, self.n*self.m/(b0 + beta_i_star))
                 self.b2[l+1] = 1*self.b2[l]
                 for i in range(self.N):
