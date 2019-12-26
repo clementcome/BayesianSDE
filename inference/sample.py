d43d0ac498fff0ed3ab828f5d2f8c780bc5ee5ae
clement <clement.come@student-cs.fr>
2019-12-26 15:15:02 +0100

    Try with -R instead of x-R

--- inference/sample.py
+++ inference/sample.py
@@ -3,8 +3,10 @@ import json
 import copy
 from tqdm import tqdm
 
+
 class StockSampler():
     """Class implementing the sampling algortithm."""
+
     def __init__(self, prices_array, tickers, dates, m):
         """
         Initializes the sampler with all the parameters needed and a level of discretization given by m
@@ -23,10 +25,11 @@ class StockSampler():
         Create the matrix x as specified in the model
         """
         prices_array = self.prices_array
-        insertion_rows = [i for i in range(1,self.n) for _ in range(self.m-1)]
+        insertion_rows = [i for i in range(
+            1, self.n) for _ in range(self.m-1)]
         x = np.insert(prices_array, insertion_rows, 0, axis=0)
         return x
-    
+
     def init_shape(self, n_iter):
         """
         Initialize the shape of the different parameters for n_iter iterations
@@ -36,48 +39,59 @@ class StockSampler():
         nbis, N = self.eta.shape
         self.etas = np.zeros((n_iter, nbis, N))
         self.Rs = np.zeros((n_iter, nbis, N))
-    
+
     def init_value(self, m0, s02, a0, b0):
         """
         Initialize the value of the different parameters
         """
         self.b2[0] = 1/np.random.gamma(a0, 1/b0, self.N)
-        self.theta[0] = np.random.normal([m0]*self.N, [np.sqrt(s02)]*self.N)
+        self.theta[0] = np.random.normal(
+            m0, np.sqrt(s02), size=self.N)
 
-    def sample(self, n_iter = 100, m0 = 0, s02 = 0.01, a0 = 4, b0 = 1):
+    def sample(self, n_iter=100, m0=0, s02=0.01, a0=4, b0=1):
         """
         Sample the different parameters for n_iter iterations
         """
         self.init_shape(n_iter)
         self.init_value(m0, s02, a0, b0)
         self.beta = []
-        tau_0 = self.n*self.m/self.b2[0]/self.b2[0]
+        tau_0 = np.full(self.N, 1/s02)
         for l in tqdm(range(n_iter)):
             for j in range(self.n-1):
-                for k in range(1,self.m):
-                    self.eta[self.m*j + k] = (1+ self.theta[l]/self.m)*self.eta[self.m*j + k -1]
-                    mR = self.R[self.m*j + k-1] + (self.x[self.m*(j+1)] - self.R[self.m*j + k-1])/(self.m - k +1)
-                    sR2 = (self.m - k)/(self.m - k + 1)/self.m *self.R[self.m*j +k-1]**2
-                    sR2 = sR2* self.b2[l]
-                    self.R[self.m*j +k] = np.random.normal(mR, np.sqrt(sR2))
-                    self.x[self.m*j +k] = self.eta[self.m*j +k] + self.R[self.m*j +k]
-            if l < n_iter -1:
-                mu_ijk = self.m*(self.x/np.roll(self.x, 1, axis=1) -1)
+                for k in range(1, self.m):
+                    self.eta[self.m*j + k] = (1 + self.theta[l] /
+                                              self.m)*self.eta[self.m*j + k - 1]
+                    mR = self.R[self.m*j + k-1] - (
+                        self.R[self.m*j + k-1])/(self.m - k + 1)
+                    sR2 = (self.m - k)/(self.m - k + 1) / \
+                        self.m * self.R[self.m*j + k-1]**2
+                    sR2 = sR2 * self.b2[l]
+                    self.R[self.m*j +
+                           k] = np.random.normal(mR, np.sqrt(sR2))
+                    self.x[self.m*j + k] = self.eta[self.m *
+                                                    j + k] + self.R[self.m*j + k]
+            if l < n_iter - 1:
+                mu_ijk = self.m * \
+                    (self.x/np.roll(self.x, 1, axis=1) - 1)
                 mu_ijk = np.delete(mu_ijk, 0, axis=0)
                 mu_i = np.sum(mu_ijk, axis=0)/(self.n*self.m)
-                tau_i = self.n/ self.b2[l]/(self.n*self.m)
-                beta_ijk = (self.x/np.roll(self.x, 1, axis=1) -1 - self.theta[l]/self.m)**2
+                tau_i = self.n / self.b2[l]/(self.n*self.m)
+                beta_ijk = (self.x/np.roll(self.x, 1,
+                                           axis=1) - 1 - self.theta[l]/self.m)**2
                 beta_ijk = np.delete(beta_ijk, 0, axis=0)
                 beta_i_star = np.sum(beta_ijk, axis=0)
-                self.beta.append(beta_i_star)              
-                self.theta[l+1]= np.random.normal((mu_i + m0)/(tau_i+tau_0), 1/(tau_i+ tau_0))        
-                proposed_b2 = 1/np.random.gamma(a0 + 1/2, self.n*self.m/(b0 + beta_i_star))
+                self.beta.append(beta_i_star)
+                self.theta[l+1] = np.random.normal(
+                    (mu_i + m0)/(tau_i+tau_0), 1/(tau_i + tau_0))
+                proposed_b2 = 1 / \
+                    np.random.gamma(
+                        a0 + 1/2, self.n*self.m/(b0 + beta_i_star))
                 self.b2[l+1] = 1*self.b2[l]
                 for i in range(self.N):
                     if proposed_b2[i] < 1:
                         self.b2[l+1][i] = proposed_b2[i]
-            self.etas[l] = 1*self.eta 
-            self.Rs[l] = 1*self.R 
+            self.etas[l] = 1*self.eta
+            self.Rs[l] = 1*self.R
 
     def save_to_json(self, file_path="samples.json"):
         """
@@ -96,4 +110,3 @@ class StockSampler():
         with open(file_path, 'w') as out_file:
             json.dump(to_save, out_file)
         print("Samples saved in {}".format(file_path))
-
